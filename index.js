99edb78b82cf9a92517fa093d909a5d004fad8ae
David <david@thejurne.com>
2020-02-12 13:45:32 -0800

    add questions to questions array

--- index.js
+++ index.js
@@ -3,7 +3,20 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const questions = [
-
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your Github username?',
+  },
+  {
+    type: 'list',
+    name: 'color',
+    message: 'What is your favorite color?',
+    choices: ['Green', 'Blue', 'Pink', 'Red'],
+    filter: function(val) {
+      return val.toLowerCase();
+    },
+  },
 ];
 
 const writeToFile = (fileName, data) => {
